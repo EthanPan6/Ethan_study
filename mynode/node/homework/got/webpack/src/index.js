8a53c1e6b0dc46f9df6a973254c0804bf7b36b20
EthanPan6 <alwaysmrp@vip.qq.com>
2019-10-24 20:12:46 +0800

    110

--- mynode/node/homework/got/webpack/src/index.js
+++ mynode/node/homework/got/webpack/src/index.js
@@ -1,4 +1,4 @@
 const $ = require('jquery')
-// const styles = require('./assets/style.css')
-const test = require('./assets/test.txt')
+const styles = require('./assets/style.css')
+const test = require('./assets/test.html')
 $('body').html(test.default)
--- mynode/node/homework/got/webpack/webpack.config.js
+++ mynode/node/homework/got/webpack/webpack.config.js
@@ -15,8 +15,24 @@ module.exports = {
         filename: 'bundle.js'//输出文件名(bundle:打包),打包后的模块文件
     },
     module: {
-        rules: [
-            { test: /\.txt$/, use: 'raw-loader' }
-        ]
+        rules: [{
+            test: /\.txt$/,
+            use: 'raw-loader'
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // 将 JS 字符串生成为 style 节点
+            }, {
+                loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+            }, {
+                loader: "sass-loader" // 将 Sass 编译成 CSS
+            }]
+        }, {
+            test: /\.html$/,
+            use: 'html-loader'
+        }]
     }
 };
