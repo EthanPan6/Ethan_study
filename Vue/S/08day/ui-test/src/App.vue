4306accde561d0ee279d8aba13a56e1f6aac3842
EthanPan6 <alwaysmrp@vip.qq.com>
2019-10-31 13:54:53 +0800

    00

---
+++ Vue/S/08day/ui-test/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Search />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Search from "./components/Search.vue";
+
+export default {
+  name: "app",
+  components: {
+    // HelloWorld
+    Search
+  }
+};
+</script>
+
+<style>
+</style>
