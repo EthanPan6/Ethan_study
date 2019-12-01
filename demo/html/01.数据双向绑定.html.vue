48d53eba3d8983288d0f1c565abc32ed7f5c37e8
EthanPan6 <alwaysmrp@vip.qq.com>
2019-12-01 15:05:28 +0800

    data

---
+++ demo/html/01.数据双向绑定.html.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div id="demo">
+      <label for="in">输入:</label>
+      <!-- <input id="in" type="text" :value="msg" /> -->
+      <input id="in" type="text" @input="change" />
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
