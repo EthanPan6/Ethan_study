c8b68de10644308a80121898f2251c197f506da1
EthanPan6 <alwaysmrp@vip.qq.com>
2020-08-23 13:47:44 +0800

    de

---
+++ Others/面试题/函数调用.js
@@ -0,0 +1,19 @@
+function Foo() {
+    getName = function () { console.log(1); };
+    console.log(this);
+
+    return this;
+}
+Foo.getName = function () { console.log(2); };
+Foo.prototype.getName = function () { console.log(3); };
+var getName = function () { console.log(4); };
+function getName() { console.log(5); }
+
+//请写出以下输出结果：
+Foo.getName(); //2
+getName();//4
+// Foo().getName();//1
+// getName();
+// new Foo.getName();
+// new Foo().getName();
+// new new Foo().getName();
