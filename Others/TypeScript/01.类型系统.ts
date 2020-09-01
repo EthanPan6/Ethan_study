e25ccafa974aa3c8091bf9071a5a195ed85c93f6
EthanPan6 <alwaysmrp@vip.qq.com>
2020-09-01 19:03:18 +0800

    zzz

--- Others/TypeScript/01.类型系统.ts
+++ Others/TypeScript/01.类型系统.ts
@@ -1,10 +1,14 @@
 let a: string;
 a = "String";
 
+let num: number = 12;
+let isOk: boolean = true;
 
 function add(x: number, y: number) {
     return x + y
 }
 
-add(1,12)
+add(1, 12)
 
+let un: undefined = undefined;//将undefined赋给了un,un无法再赋除null和undefined的其他值
+let nu: null = null;//将null赋给了un,un无法再赋除null和undefined的其他值
--- Others/TypeScript/dist/01.类型系统.js
+++
@@ -1,3 +0,0 @@
-var a;function add(a,d){return a+d}a="String",add(1,12); y;
-}
-add(1, 12);
