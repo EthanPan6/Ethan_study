57b7ce9a4110d0522fbfada37eef7fc7d781dd66
EthanPan6 <alwaysmrp@vip.qq.com>
2020-09-01 20:04:31 +0800

    shuju

---
+++ Others/Test/constructor.js
@@ -0,0 +1,11 @@
+let a = ''.constructor;
+let b = 2..constructor;
+let c = true.constructor;
+// let d=null.constructor;
+// let e = undefined.constructor;
+let f = Symbol().constructor;
+let g = {}.constructor;
+let h = new Function().constructor;
+let i = [].constructor;
+
+console.log(a, b, c, f, g, h, i)
