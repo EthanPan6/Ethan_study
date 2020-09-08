232a9077537eb5b8cb4c6550c257561620fa1267
EthanPan6 <alwaysmrp@vip.qq.com>
2020-09-08 21:30:13 +0800

    nnn

--- Node/模块介绍/01.exports.js
+++ Node/模块介绍/01.exports.js
@@ -2,8 +2,21 @@
 
 let a = "exports";
 
-// exports.a = a;
-
-module.exports = {
-    a
+let fn = v => {
+    console.log(v);
 }
+// exports.a = a;
+// module.exports.a = a
+module.exports = {
+    a,
+    fn
+}
+
+/*
+使用exports时只能导出单个属性
+
+
+module.exports 可以导出单个属性,也可以导出整个对象
+
+
+*/
--- Node/模块介绍/02.require.js
+++ Node/模块介绍/02.require.js
@@ -1,4 +1,4 @@
 //模块的引入
 let a = require("./01.exports")
 
-console.log(a);//{ a: 'exports' }
+console.log(a.fn(5)); //{ a: 'exports' }
