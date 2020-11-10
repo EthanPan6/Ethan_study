a3369d49e25a8f84357f5ff7c7fe6b891e0279a8
EthanPan6 <alwaysmrp@vip.qq.com>
2020-11-10 16:04:26 +0800

    console

--- utils.js
+++ utils.js
@@ -76,6 +76,28 @@ function zeroize(value) {
 }
 // console.log(zeroize(8));
 
+//控制台防调试
+var noDebug = (() => {
+    setInterval(function () {
+        check()
+    }, 4000);
+    var check = function () {
+        function doCheck(a) {
+            if (("" + a / a)["length"] !== 1 || a % 20 === 0) {
+                (function () { }
+                ["constructor"]("debugger")())
+            } else {
+                (function () { }
+                ["constructor"]("debugger")())
+            }
+            doCheck(++a)
+        }
+        try {
+            doCheck(0)
+        } catch (err) { }
+    };
+    check();
+})()
 
 
 // export {
