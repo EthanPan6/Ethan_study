6aef323f7a6b8b9de1bad46fbb4d37a68024dccc
EthanPan6 <alwaysmrp@vip.qq.com>
2020-11-29 21:33:22 +0800

    write file

--- Node/02Advance/02fs/index.js
+++ Node/02Advance/02fs/index.js
@@ -1,11 +1,77 @@
 const fs = require('fs');
 
 // fs.stat检测该路径是目录还是文件
-fs.stat('./html', (err, data) => {
+// fs.stat('./html', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log(data.isDirectory());//true
+//     console.log(data.isFile());//false
+// })
+// // fs.mkdir创建目录(重要)
+// fs.mkdir('./css', err => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log('创建成功');
+// })
+
+// fs.writeFile('./html/text', 'Hello World', err => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log('写入成功');
+// })
+
+// fs.appendFile('./html/text', '二次写入', err => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log('二次写入成功');
+// })
+// // 读取文件,读出的是二进制文件,可以通过toString方法变成字符串
+// fs.readFile('./html/text', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log(data.toString());
+// })
+
+// fs.readdir('./html', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log(data);
+// })
+
+// fs.rename可以重命名和移动文件
+// fs.rename('./html/text', './css/style.css', err => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log('成功');
+// })
+
+//fs.rmdir删除目录,如果目标目录下还有文件则无法删除
+fs.rmdir('./html', err => {
     if (err) {
         console.log(err);
         return
     }
-    console.log(data.isDirectory());//true
-    console.log(data.isFile());//false
+    console.log('删除成功');
+})
+//fs.unlink删除文件
+fs.unlink('./css/style.css', err => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log('删除成功');
 })
--- Node/02Advance/readme.md
+++ Node/02Advance/readme.md
@@ -29,3 +29,80 @@ res.write('<head><meta charset="UTF-8"></head>')
 ```
 使用
 ```cmd
+
+## fs文件系统模块
+
+fs 模块可用于与文件系统进行交互,所有的文件系统操作都具有同步的、回调的、以及基于 promise 的形式。
+
+### 文件处理实例
+1. 同步处理:同步的形式会阻塞 Node.js 事件循环和进一步的 JavaScript 执行，直到操作完成。 异常会被立即地抛出，可以使用 try…catch 处理，也可以冒泡。
+
+```js
+try {
+  fs.unlinkSync('文件');
+  console.log('已成功删除文件');
+} catch (err) {
+  // 处理错误
+}
+```
+
+2. 回调:异步的形式总是把完成回调作为其最后一个参数。 传给完成回调的参数取决于具体方法，但第一个参数总是预留给异常。 如果操作被成功地完成，则第一个参数会为 null 或 undefined
+
+```js
+fs.unlink('文件', (err) => {
+  if (err) throw err;
+  console.log('已成功地删除文件');
+});
+```
+
+3. promise:基于 promise 的操作会返回 Promise
+
+```js
+(async function(path) {
+  try {
+    await fs.unlink(path);
+    console.log(`已成功地删除文件 ${path}`);
+  } catch (error) {
+    console.error('出错：', error.message);
+  }
+})('文件');
+```
+
+### fs.stat 检测状态
+
+检测该路径是目录还是文件
+语法`fs.stat(path[, options], callback)`
+
+```js
+fs.stat('./html', (err, data) => {
+    if (err) throw err;
+    console.log(data.isDirectory());//true
+    console.log(data.isFile());//false
+})
+```
+
++ 若要只检查文件是否存在，但没有更多的操作，则建议使用 fs.access()。
+
+### fs.writeFile写入文件
++ 当 file 是文件名时，则异步地写入数据到文件（如果文件已存在，则覆盖文件）。 data 可以是字符串或 buffer。
++ 如果 data 是普通的对象，则它必须具有自身的 toString 函数属性
+`fs.writeFile(file, data[, options], callback)`
+
+```fs
+fs.writeFile('./html/text', 'Hello World', err => {
+    if (err) throw err;
+    console.log('写入成功');
+})
+```
+不等待回调就对同一个文件多次使用 fs.writeFile() 是不安全的。 对于这种情况，建议使用 fs.createWriteStream()。
+### 删除文件
+
+语法`fs.unlink(path, callback)`
+
+```js
+fs.unlink('文件', (err) => {
+  if (err) throw err;
+  console.log('已成功地删除文件');
+});
+```
+
