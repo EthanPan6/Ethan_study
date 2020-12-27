b30b15a017001d421ea88907afa4dc444266a35f
EthanPan6 <alwaysmrp@vip.qq.com>
2020-12-27 21:30:20 +0800

    fff

--- Daily/02.数据响应/vue2.js
+++ Daily/02.数据响应/vue2.js
@@ -3,18 +3,19 @@ function reactive(data) {
         return data
     }
 
-
     Object.keys(data).forEach(function (key) {
-        let value = data[key]
+        let value = data[key];
+        reactive(value)
         Object.defineProperty(data, key, {
             emumerable: false,
             configurable: true,
             get: () => {
+                console.log('触发get');
                 return value
             },
             set: newVal => {
+                console.log('触发set');
                 if (newVal !== value) {
-                    effective()
                     value = newVal
                 }
             }
@@ -23,4 +24,20 @@ function reactive(data) {
     })
     return data
 }
+
+let o = {
+    name: 'jsck',
+    age: 18,
+    job: 'learner',
+    like: {
+        a: 1,
+        b: 2
+    }
+}
+let data = reactive(o)
+data.age = 23
+data.job
+o.name = 'marry'
+o.like.c = 3
+o.like
 
